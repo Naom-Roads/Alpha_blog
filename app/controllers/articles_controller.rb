00e1dcc3164bcfc3d18366e336c9ef6b73ba9c3c
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2020-11-05 22:10:23 +0100

    Added edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,37 @@ class ArticlesController < ApplicationController
     @articles = Article.all
     end
 
-end
+    def new 
+        @article = Article.new
+   
+    end
+
+    def edit
+        
+        @article = Article.find(params[:id])
+    
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if  @article.save
+            flash[:notice] = 'Article was created successfull.'
+            redirect_to @article
+        else 
+            render 'new'
+        end
+    end
+
+    def update
+
+      @article = Article.find(params[:id])
+     if  @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = 'Article was updated successfully'
+      redirect_to @article
+    else
+        render 'edit'
+     
+    end
+    end
+
+  end 
