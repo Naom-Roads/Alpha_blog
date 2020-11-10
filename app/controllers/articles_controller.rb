40b18dce29395af508387b8b3ae0bbe92ce8ce3c
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2020-11-10 14:33:49 +0100

    Refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,50 +1,50 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
 
-    def show
-      @article = Article.find(params[:id])
-    end
+  def show; end
 
-    def index
+  def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit; end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article was created successfull.'
+      redirect_to @article
+    else
+      render 'new'
     end
+  end
 
-    def new 
-        @article = Article.new
-   
-    end
-
-    def edit
-        
-        @article = Article.find(params[:id])
-    
-    end
-
-    def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
-        if  @article.save
-            flash[:notice] = 'Article was created successfull.'
-            redirect_to @article
-        else 
-            render 'new'
-        end
-    end
-
-    def update
-
-      @article = Article.find(params[:id])
-     if  @article.update(params.require(:article).permit(:title, :description))
+  def update
+    if @article.update(article_params)
       flash[:notice] = 'Article was updated successfully'
       redirect_to @article
     else
-        render 'edit'
-     
-    end
-    end
+      render 'edit'
 
-    def destroy 
-        @article = Article.find(params[:id])
-        @article.destroy 
-        redirect_to articles_path 
-    end
+   end
+  end
 
-  end 
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+      end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+end
