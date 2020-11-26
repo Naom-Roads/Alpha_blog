2644b261c2435baa7a3d394630bc34f6b0b3abdb
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2020-11-26 18:10:53 +0100

    Added Signup Page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+ 
+  resources :users, except: [:new]
   
   # we don'y need to fo this now because all of the routes are exposed now
   # only: [:show, :index, :new, :create, :edit, :update]
