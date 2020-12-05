d6e9d6e85d34e2a47bf30b37a05be48adf4ac649
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2020-12-05 16:10:01 +0100

    Added route for login, sessions controller and html.erb login form

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
- 
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+   
+  # Login route will not be a resources restful route because it wont hit the database
+  #we have to manually create the route
+
+
   
   # we don'y need to fo this now because all of the routes are exposed now
   # only: [:show, :index, :new, :create, :edit, :update]
