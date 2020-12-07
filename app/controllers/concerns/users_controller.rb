061622827a81ac0fc9a69387c0c5a0247c038b8e
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2020-12-07 14:23:28 +0100

    Added Current_user and logged_in? helper method, and modified navigation partial so that users can login and log out. Also made a change to sign up so that new users are redirected to profile

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -12,12 +13,9 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = 'Your account was succesfully updated'
       redirect_to @user
@@ -29,6 +27,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
@@ -41,5 +40,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,12 +15,12 @@ class SessionsController < ApplicationController
       flash.now[:alert] = 'There was something wrong with your login details'
       render 'new'
     end
+  end
 
     def destroy
         session[:user_id] = nil
         flash[:notice] = "logged out"
         redirect_to root_path
-
     end
-   end
+
 end
