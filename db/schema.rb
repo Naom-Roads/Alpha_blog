b32720bcc97ba3653da57c3a7901f930dd345419
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2020-12-15 15:35:11 +0100

    Added unit tests and category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_130029) do
+ActiveRecord::Schema.define(version: 2020_12_15_133208) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2020_12_12_130029) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+   t.string "name"
+   t.timestamps  
   end
 
   create_table "users", force: :cascade do |t|
