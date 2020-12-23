93b950e57aa1488245fcc2b0dd26bf5a1c74d6dc
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2020-12-23 13:19:35 +0100

    Added pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,11 +13,12 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def index; end
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
 
   def show
-    @category = Category.find(params[:id]) 
-
+    @category = Category.find(params[:id])
 end
 
   private
